a62050eafc396ffb1ec97584cc10588dbd6502b0
koji <iisparkplugiiog@gmail.com>
2020-03-25 10:53:40 +0800

    SUBJECT: Screenshot and Download PR done
    BODY: Used the link in the repo to obtain the required parameters using
    the octokit. Personal Access Token was generated for authenticated
    request, PATs are generated in github account
    TASK: Need to check for bugs and added features. Need to implement for
    PHP and to utilize .env
    Notes: Check for bugs!!

---
+++ utilities/screenshot.js
@@ -0,0 +1,13 @@
+// const puppeteer = require('puppeteer');
+// const fs = require('fs');
+
+// const config = {
+//     page_directory: '' || process.env.PAGES,
+// }
+
+// // (async () => {
+// //     const browser = await puppeteer.launch({headless: true});
+// //     const page = await browser.newPage();
+// // })
+
+
